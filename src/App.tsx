04f080983292d2a014c57abf2f4bc906c6cff49e
Navid <Navid.dargahi@gmail.com>
2026-02-06 16:44:23 +0330

    Added sort

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import data from './data.json'
 
 export function App() {
   const newData: ChunkType[] = data;
+  newData.sort((a, b) => (a!.artist + a!.date).toUpperCase().localeCompare((b!.artist + b!.date).toUpperCase()))
   const chunks = []
   for (let i = 0; i < newData.length % 7; i++) {
     newData.push(null)
